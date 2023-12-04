477f039f5972cbcdf078035a4faaee6623dd45f5
jonnyhak <jonnyhak@gmail.com>
2023-12-04 13:18:48 -0500

    new project

---
+++ server/databse.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE pernhelpdesk;
+
+CREATE TABLE ticket(
+    ticket_id SERIAL PRIMARY KEY,
+    description VARCHAR(255)
+);
+
---
+++ server/index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//ROUTES//
+
+//create a ticket
+app.post("/tickets", async(req, res) => {
+    try {
+        const { description, email, userName, status, reply} = req.body
+        const newTicket = await pool.query(
+            "INSERT INTO ticket (description, email, userName, status, reply) VALUES($1, $2, $3, $4, $5) RETURNING *",
+            [description, email, userName, status, reply]
+        );
+
+        res.json(newTicket.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+//get all tickets
+
+app.get("/tickets", async(req, res) => {
+    try {
+        const allTickets = await pool.query("SELECT * FROM ticket");
+        res.json(allTickets.rows);
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+//get a ticket
+
+app.get("/tickets/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const ticket = await pool.query("SELECT * FROM ticket WHERE ticket_id = $1", [id])
+
+        res.json(ticket.rows)
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+//update a ticket
+
+app.put("/tickets/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const { reply, status } = req.body;
+        const updateTicket = await pool.query("UPDATE ticket SET reply = $1, status = $2 WHERE ticket_id = $3", [reply, status, id]);
+    
+        res.json("Ticket was updated!")
+    } catch (error) {
+        console.error(err.message)
+    }
+})
+
+//delete a ticket
+
+app.delete("/tickets/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deleteTicket = await pool.query("DELETE FROM ticket WHERE ticket_id = $1", [
+        id
+      ]);
+      res.json("Ticket was deleted!");
+    } catch (err) {
+      console.log(err.message);
+    }
+});
+
+
+
+app.listen(5000, () => {
+    console.log("server has started on port 5000")
+})
