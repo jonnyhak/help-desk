e470fd4bbdaf01395a52188006a57ba6e79c7671
jonnyhak <jonnyhak@gmail.com>
2023-12-04 23:23:28 -0500

    comment in database.sql

--- databse.sql
+++ databse.sql
@@ -1,7 +1,11 @@
-CREATE DATABASE pernhelpdesk;
+-- CREATE DATABASE pernhelpdesk;
 
 CREATE TABLE ticket(
     ticket_id SERIAL PRIMARY KEY,
-    description VARCHAR(255)
+    description VARCHAR(255),
+    email VARCHAR(255), 
+    userName VARCHAR(255), 
+    status VARCHAR(255), 
+    reply VARCHAR(255)
 );
 
