d0eb3ae26d3af5105006645dc7f2c22616bc1b89
jonnyhak <jonnyhak@gmail.com>
2023-12-04 21:52:00 -0500

    ready for deployment

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+//process.env.PORT
+//process.env.NODE-ENV = > production or undefined
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static("./client/build"));
+
+if (process.env.NODE_ENV === "production") {
+    //server static content
+    //npm run build
+    app.use(express.static(path.join(__dirname, "client/build")));
+}
+
+console.log(__dirname);
+console.log(path.join(__dirname, "cliend/build"))
 
 //ROUTES//
 
@@ -76,8 +92,10 @@ app.delete("/tickets/:id", async (req, res) => {
     }
 });
 
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"))
+})
 
-
-app.listen(5000, () => {
-    console.log("server has started on port 5000")
+app.listen(PORT, () => {
+    console.log(`server has started on port ${PORT}`)
 })
